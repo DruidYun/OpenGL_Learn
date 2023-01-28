ea116ff707892c263fd904519cfdc27259b1f243
DruidYun <2915167442@qq.com>
2023-01-28 22:31:02 +0800

    光照纹理

--- OpenGL_Learn/sample_2_4LightingMaps/basicLighting.fs
+++ OpenGL_Learn/sample_2_4LightingMaps/basicLighting.fs
@@ -31,7 +31,7 @@ void main()
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
     
     FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
