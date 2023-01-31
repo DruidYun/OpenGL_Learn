0b09793655b0d6c79ed318e4aced6f72c9bcb55d
DruidYun <2915167442@qq.com>
2023-01-31 18:06:34 +0800

    处理Assimp版本管理

--- Assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/MyProject/OpenGL/Assimp/src/assimp-3.1.1/contrib/zlib
-# Build directory: D:/MyProject/OpenGL/Assimp/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
